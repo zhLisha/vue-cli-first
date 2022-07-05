cf1cb8c1c78a41ca1e6fa7a5ff89e82696d6f30d
zhLisha <lisazhou1709@gmail.com>
2022-07-05 16:02:09 +0200

    cahnged name

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <HelloWorld msg="La mia prima app con Vue CLI"/>
+    <Title msg="La mia prima app con Vue CLI"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Title from './components/Title.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Title
   }
 }
 </script>
